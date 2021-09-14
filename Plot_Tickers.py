3f091b8a61120e6809947d7e476c830a3633facd
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2021-09-14 15:53:43 -0700

    Update Plot_Tickers.py

--- Plot_Tickers.py
+++ Plot_Tickers.py
@@ -4,21 +4,20 @@ import yfinance
 from pandas_datareader import data as pdr
 import streamlit as st
 
-today = dt.date.today().strftime('%Y-%m-%d')
-end = today
+end = dt.date.today().strftime('%Y-%m-%d') #Set end date for collection
 
 st.title("Tickers Historical Data")
-start = st.text_input("Input start date here:", '2020-01-01')
-tickers = st.text_input("Input tickers here:",'AAPL')
+start = st.text_input("Input start date here:", '2020-01-01') #Allow user to chose own start date
 
+tickers = st.text_input("Input tickers here:",'AAPL') #Allow user to chose ticker symbol
+
+#Collect Data
 yfinance.pdr_override()
 data = pd.DataFrame()
-
 data = pdr.get_data_yahoo(tickers, start=start, end=end)['Adj Close']
-data = data.reset_index()
-pd.to_datetime(data['Date'], format = '%Y-%m-%d')
-data = data.set_index('Date')
 data = data.iloc[::-1]
+
+#Display Data
 st.header('Adjusted Closing Values per Day')
 st.write(data)
 st.line_chart(data)
