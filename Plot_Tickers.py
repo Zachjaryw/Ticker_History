ae2512c064461e06206378b228f8253211cd9d95
Zachjaryw <85905928+Zachjaryw@users.noreply.github.com>
2021-09-14 23:03:38 -0700

    Update Plot_Tickers.py

--- Plot_Tickers.py
+++ Plot_Tickers.py
@@ -65,9 +65,9 @@ elif data.shape[1] > 2:
 #Display Accelaration Graph
 st.header(f'Acceleration Graph of Closing Values per {breakdown}')
 if ct == True:
-  st.write(change)
   change = change.set_index('Date')
   change.columns = data.columns
+  st.write(change)
   for i in range(change.shape[1]):
     st.bar_chart(change.iloc[:,i])
 else:
